77322a86992da6fc8b958c502cf0702a1eac49b5
Jake Prather <jakehp@zoho.com>
2019-05-16 14:38:42 -0500

    refactor logger - support no config passed, create constants for ease of use and readability, remove string literal usage for log levels

--- logger.js
+++ logger.js
@@ -1,12 +1,20 @@
+const chalk = require('chalk');
 const LEVELS = {
     INFO: 'info',
     WARN: 'warning',
     ERROR: 'error',
     DEBUG: 'debug'
 };
+const TRANSPORT_ACTIONS = {};
+TRANSPORT_ACTIONS[LEVELS.INFO] = (message) => { console.log(chalk.green(message)); }
+TRANSPORT_ACTIONS[LEVELS.WARN] = (message) => { console.log(chalk.yellow(message)); }
+TRANSPORT_ACTIONS[LEVELS.ERROR] = (message) => { console.log(chalk.red(message)); }
+TRANSPORT_ACTIONS[LEVELS.DEBUG] = (message) => { console.log(chalk.blue(message)); }
+const DEFAULT_ROOT = 'root';
+const DEFAULT_LEVEL = LEVELS.INFO;
 
-function logger(config) {
-    this.root = config.root || 'root';
+function logger(config = {}) {
+    this.root = config.root || DEFAULT_ROOT;
 
     if (config.format) {
         this.format = config.format;
@@ -19,31 +27,22 @@ function logger(config) {
 
 logger.prototype = {
     log(data, level) {
-        this.level = level;
-        this.data = data;
-
-        const logObj = this.createLogObject();
-
+        const logObj = this.createLogObject(data, level);
         const message = this.format(logObj);
-
         this.transport(level, message);
     },
 
-    createLogObject() {
+    createLogObject(data, level) {
         let rootObj;
 
         if (this.root) {
             rootObj = { root: this.root };
         }
 
-        const data =
-            typeof this.data == 'string' ? { message: this.data } : this.data;
-
-        const logObj = Object.assign(rootObj, data, {
-            level: this.level || 'info'
+        const adjData = typeof data === 'string' ? { message: data } : data;
+        return Object.assign(rootObj, adjData, {
+          level: level || DEFAULT_LEVEL
         });
-
-        return logObj;
     },
 
     format(logObj) {
@@ -51,17 +50,10 @@ logger.prototype = {
     },
 
     transport(level, message) {
-        const chalk = require('chalk');
-
-        if (level == 'error') {
-            console.log(chalk.red(message));
-        } else if (level == 'warning') {
-            console.log(chalk.yellow(message));
-        } else if (level == 'debug') {
-            console.log(chalk.blue(message));
+        if (TRANSPORT_ACTIONS[level]) {
+            TRANSPORT_ACTIONS[level](message);
         } else {
-            //the default is info
-            console.log(chalk.green(message));
+            TRANSPORT_ACTIONS[DEFAULT_LEVEL](message);
         }
     }
 };
